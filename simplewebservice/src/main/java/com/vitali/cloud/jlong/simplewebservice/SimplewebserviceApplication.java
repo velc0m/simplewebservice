f2fae9cb571c081bd09e8167db2d53361f060ee9
Vitali Litvinenka <lvitali@setplex.com>
2018-12-01 13:13:39 +0300

    Initial commit

---
+++ simplewebservice/src/main/java/com/vitali/cloud/jlong/simplewebservice/SimplewebserviceApplication.java
@@ -0,0 +1,12 @@
+package com.vitali.cloud.jlong.simplewebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplewebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplewebserviceApplication.class, args);
+    }
+}
---
+++ simplewebservice/src/test/java/com/vitali/cloud/jlong/simplewebservice/SimplewebserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vitali.cloud.jlong.simplewebservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimplewebserviceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
