f3ce72ce3add3359f0a7804f45a8ebca7266b656
Vitali Litvinenka <lvitali@setplex.com>
2018-12-02 09:47:08 +0300

    Added Cat entity

--- simplewebservice/src/main/java/com/vitali/cloud/jlong/simplewebservice/SimplewebserviceApplication.java
+++ simplewebservice/src/main/java/com/vitali/cloud/jlong/simplewebservice/SimplewebserviceApplication.java
@@ -1,7 +1,14 @@
 package com.vitali.cloud.jlong.simplewebservice;
 
+import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @SpringBootApplication
 public class SimplewebserviceApplication {
@@ -9,4 +16,26 @@ public class SimplewebserviceApplication {
     public static void main(String[] args) {
         SpringApplication.run(SimplewebserviceApplication.class, args);
     }
+}
+
+@Entity
+@Data
+class Cat {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String name;
+
+    Cat() {
+    }
+
+    Cat(String name) {
+        this.name = name;
+    }
+}
+
+@RepositoryRestResource
+interface CatRepository extends JpaRepository<Cat, Long> {
 }
